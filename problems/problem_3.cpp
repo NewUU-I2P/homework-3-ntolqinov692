7ec5ca0507ebbd4662e79cd8b89777ea44e36df0
Nurmuhammad To'lqunov <n.tolqunov@newuu.uz>
2023-11-13 11:50:57 +0500

    HW3

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,7 +2,26 @@
 
 std::string problemSolution3(float height, char S) {
     // write your code here
+    std::string height1;
+    if(S == 'M'){
+        if(height < 1.70)
+            height1 = "Short";
+        if(height >= 1.70 && height < 1.85)
+            height1 = "Normal";
+        if(height >= 1.85)
+            height1 = "Tall";
+    }
+    if(S == 'F'){
+        if(height < 1.60)
+            height1 = "Short";
+        if(height >= 1.60 && height < 1.75)
+            height1 = "Normal";
+        if(height >= 1.75)
+            height1 = "Tall";
+    }
+
 
     // use return to return your result
     // make use of control flow statements
+    return height1;
 }
